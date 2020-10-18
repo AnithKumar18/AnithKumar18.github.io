2ddbd70e026012dcf73ce8b451c7e4c0f992eba7
Anith Kumar <anithkumartp016631@gmail.com>
2020-10-18 20:23:12 +0800

    init save

--- PROJECT2020/Main.js
+++ PROJECT2020/Main.js
@@ -3,7 +3,12 @@ const selectElement = function (element) {
 };
   
 let header = document.querySelector('.header'); 
-let hamburgerMenu = document.querySelector('.hamburger-menu'); 
+let hamburgerMenu = document.querySelector('.hamburger-menu');  
+ 
+window.addEventListener('scroll', function () {  
+ let windowPosition = window.scrollY > 0; 
+ header.classList.toggle('active', windowPosition)
+})
  
 hamburgerMenu.addEventListener('click', function(){ 
     header.classList.toggle('menu-open');
