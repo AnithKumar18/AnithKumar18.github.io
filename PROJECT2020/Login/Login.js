03cd231b94c56330a9af9a1417c52e50d31559ac
Anith Kumar <anithkumartp016631@gmail.com>
2020-10-21 13:06:07 +0800

    init

---
+++ PROJECT2020/Login/Login.js
@@ -0,0 +1,31 @@
+function BookNow(guestName,guestEmail,guestPax){ 
+    let url = 'https://api.sheety.co/0296a26a9104fbbb400d264d4ed644b1/bookingApp/registration';
+    let body = {
+      registration: {
+        name:guestName, 
+        email:guestEmail, 
+        pax:guestPax
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: { 
+       "Content-Type":"application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+      console.log(json.registration); 
+      document.getElementById("bookMsg").innerHTML = json.registration.name +  " successfully added"; 
+      GetBookings();
+    });
+  }  
+   
+  document.getElementById("bookNow").addEventListener("click", function(){ 
+   let guestName = document.getElementById("guestName").value; 
+   let guestEmail = document.getElementById("guestEmail").value; 
+   let guestPax = document.getElementById("guestPax").value; 
+    BookNow(guestName,guestEmail,guestPax);
+  });
