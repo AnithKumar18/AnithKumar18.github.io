97b8e35ec2c26cd6202e63d6f93d53aab3fee3c1
Anith Kumar <anithkumartp016631@gmail.com>
2020-10-26 21:21:55 +0800

    init save

---
+++ Table-Booking/scripts/booking.js
@@ -0,0 +1,35 @@
+function BookNow(guestName,guestEmail,guestPax,guestRemark){ 
+    let url = 'https://api.sheety.co/aa2f7b5a49d9fbeb0e14c8073ac2fe02/tableBooking/booking';
+    let body = {
+      booking: {
+        name:guestName, 
+        email:guestEmail, 
+        pass:guestPax, 
+        remark:guestRemark
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: { 
+       "Content-Type":"application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+      console.log(json.booking); 
+      document.getElementById("bookMsg").innerHTML = json.booking.name +  " successfully added"; 
+      GetBookings();
+    });
+  }  
+   
+  document.getElementById("bookNow").addEventListener("click", function(){ 
+   let guestName = document.getElementById("guestName").value; 
+   let guestEmail = document.getElementById("guestEmail").value; 
+   let guestPax = document.getElementById("guestPax").value;   
+   let guestRemark = document.getElementById("guestRemark").value;  
+
+
+    BookNow(guestName,guestEmail,guestPax,guestRemark);
+  });
