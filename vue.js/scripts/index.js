eeb6c180d794dad150f3513bf303873fb1aaa1a4
Anith Kumar <anithkumartp016631@gmail.com>
2020-10-30 20:44:57 +0800

    init change

--- vue.js/scripts/index.js
+++ vue.js/scripts/index.js
@@ -19,4 +19,16 @@ var app3 = new Vue ({
     data: { 
         seen:true
     }
+}); 
+ 
+var app4 = new Vue ({ 
+    el:"#app-4", 
+    data: { 
+        todos: [ 
+            {text: "Learn HTML = Done"}, 
+            {text: "Learn CSS = Done"},
+            {text: "Learn JS = Done"},
+            {text: "Learn Vue = In Progress"}
+        ]
+    }
 });
